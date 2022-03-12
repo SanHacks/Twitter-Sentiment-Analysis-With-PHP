61a9acdc12e786ecda4114cbc389c5434018e6de
San Sifhufhi <sifhufhisg@gmail.com>
2022-03-12 23:59:59 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Configure your Datumbox API Key. Get yours at http://www.datumbox.com/apikeys/view/
+define('DATUMBOX_API_KEY', 'ae6f11426535edaf080471d3f422f8ce');
+
+// Configure your OAuth settings from your application at https://dev.twitter.com/apps
+
+define('TWITTER_CONSUMER_KEY', 'yqqqZRT6dy4YXyaUz23P5gmIW');
+define('TWITTER_CONSUMER_SECRET', 'PvDZiXDAj0BUwb9SPGy45FZwwkx1pzbud3IP7HL12MgHAw0KZH');
+define('TWITTER_ACCESS_KEY', '2217127438-fFX27rHqGI3pjFH9uj3TvoBfCYgLhxXmUNbHHxn');
+define('TWITTER_ACCESS_SECRET', 'DLiIcnzhuQRSeP5b5nZcoj3U0otlgIMfMlJd2AFBBphko'); 
