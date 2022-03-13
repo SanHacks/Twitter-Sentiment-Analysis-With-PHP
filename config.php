44fd9337360dade37eb22a971db564fda85cadf2
San Sifhufhi <sifhufhisg@gmail.com>
2022-03-13 02:38:37 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
 // Configure your Datumbox API Key. Get yours at http://www.datumbox.com/apikeys/view/
-define('DATUMBOX_API_KEY', 'ae6f11426535edaf080471d3f422f8ce');
+define('DATUMBOX_API_KEY', 'YOUR API KEY HERE');
 
 // Configure your OAuth settings from your application at https://dev.twitter.com/apps
 
-define('TWITTER_CONSUMER_KEY', 'yqqqZRT6dy4YXyaUz23P5gmIW');
-define('TWITTER_CONSUMER_SECRET', 'PvDZiXDAj0BUwb9SPGy45FZwwkx1pzbud3IP7HL12MgHAw0KZH');
-define('TWITTER_ACCESS_KEY', '2217127438-fFX27rHqGI3pjFH9uj3TvoBfCYgLhxXmUNbHHxn');
-define('TWITTER_ACCESS_SECRET', 'DLiIcnzhuQRSeP5b5nZcoj3U0otlgIMfMlJd2AFBBphko'); 
+define('TWITTER_CONSUMER_KEY', 'YOUR CONSUMER KEY HERE');
+define('TWITTER_CONSUMER_SECRET', 'YOUR SECRET KEY HERE');
+define('TWITTER_ACCESS_KEY', 'YOUR TWITTER ACCESS KEY HERE');
+define('TWITTER_ACCESS_SECRET', 'YOUR ACCESS SECRET HERE'); 
