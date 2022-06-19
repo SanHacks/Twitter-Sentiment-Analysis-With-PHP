fa093ec66e24899f9878053e44b789a90012a162
San Sifhufhi <sifhufhisg@gmail.com>
2022-06-19 23:48:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,7 @@ Build by Gundo Sifhufhi
  =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
-<!--
+
 =========================================================
 * Paper Dashboard 2 - v2.0.1
 =========================================================
