33f9f1555509ea5a5f3e86f5ccd3352513d5e7ca
San Sifhufhi <sifhufhisg@gmail.com>
2022-06-19 23:47:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!--
-=========================================================
-* Paper Dashboard 2 - v2.0.1
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-2
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
 <!--
 =========================================================
 * PHP TWITTER SENTIMENT ANALYSIS   v0.0.1
@@ -21,6 +8,20 @@ Coded by www.creative-tim.com
 * Copyright 2021 Gundo San Sifhufhi (https://gundo.ml)
 
 Build by Gundo Sifhufhi
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!--
+=========================================================
+* Paper Dashboard 2 - v2.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-2
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
 
  =========================================================
 
